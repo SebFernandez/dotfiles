e0e0e6e7fe2643218020c389111480fee47357a0
SebFernandez <seba.fernandez95@gmail.com>
2020-09-29 18:14:22 -0300

    Fixed when ram usage was below Gibibytes.

--- ram.sh
+++ ram.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mem=$(free -h | awk '/^Mem:/ {print $3}' | cut -c1-3 | awk '{print ($1 * 100) / 7,7}' | awk '{print int($1)}')
+mem=$(free -h | awk '/^Mem:/ {print $3}' | cut -c1-3 | awk '{ if ($1 < 7) {print (($1 * 100) / 7,7)} else {print (($1 /1024)* 100) / 7,7} }' | awk '{printf "%.0f", $1}')
 echo "${mem}% "
