96b5ad1018f7590062a82e10660d83d32910a585
Sebastian Fernandez <seba.fernandez95@gmail.com>
2021-01-27 17:36:10 -0300

    Fix, now works in a generic way.

--- ram.sh
+++ ram.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-mem=$(free -h | awk '/^Mem:/ {print $3}' | cut -c1-3 | awk '{ if ($1 < 7) {print (($1 * 100) / 7,7)} else {print (($1 /1024)* 100) / 7,7} }' | awk '{printf "%.0f", $1}')
-echo "${mem}% "
+mem=$(free -h | awk '/^Mem:/ {print $2" "$3}' | sed 's/[Gi]//g' | awk '{print (($2 * 100) / $1)}' | awk '{printf "%.0f", $1}')
+echo -e "${mem}%"
